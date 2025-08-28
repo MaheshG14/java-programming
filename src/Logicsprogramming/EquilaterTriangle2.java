18252bf9e40addfd14e3fdf54ff3260668a4fe96
MaheshG14 <maheshgajengi13@gmail.com>
2025-08-28 14:09:49 +0530

    Programming

--- src/Logicsprogramming/EquilaterTriangle2.java
+++ src/Logicsprogramming/EquilaterTriangle2.java
@@ -9,7 +9,7 @@ public class EquilaterTriangle2
 		
 		 int consoleSize= 160;
 		 
-		 for(int i=1;i<=10;i++)
+		  for(int i=1;i<=10;i++)
 		 {
 			 for(int j=1;j<=consoleSize/2-i ;j++)
 			 {
@@ -20,6 +20,7 @@ public class EquilaterTriangle2
 					  System.out.print("* ");
 				 } 
 				 System.out.println(); 
+				 
 				 
 		 }
 	}
